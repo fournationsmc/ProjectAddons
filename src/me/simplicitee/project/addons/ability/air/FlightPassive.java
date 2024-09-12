a9abea65d98bf9028fffab583f7e15aa0296f5c0
CozmycDev <172064888+CozmycDev@users.noreply.github.com>
2024-09-12 15:09:41 -0400

    Fix players unable to pickup items under certain circumstances

--- src/me/simplicitee/project/addons/ability/air/FlightPassive.java
+++ src/me/simplicitee/project/addons/ability/air/FlightPassive.java
@@ -5,13 +5,23 @@ import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.FlightAbility;
 import com.projectkorra.projectkorra.ability.PassiveAbility;
 import me.simplicitee.project.addons.ProjectAddons;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 public class FlightPassive extends FlightAbility implements AddonAbility, PassiveAbility {
 
-	private boolean toggled = false, active = false, pickup;
-	private float original = 0.8f, speed, flySpeed, startSpeed, maxSpeed, acceleration;
+	private boolean toggled = false;
+	private boolean active = false;
+	private float original = 0.8f;
+    private float speed;
+
+    private final float flySpeed;
+    private final float startSpeed;
+    private final float maxSpeed;
+    private final float acceleration;
 	
 	public FlightPassive(Player player) {
 		super(player);
@@ -26,31 +36,33 @@ public class FlightPassive extends FlightAbility implements AddonAbility, Passiv
 
 	@Override
 	public void progress() {
-		if (!bPlayer.isElementToggled(Element.AIR)) {
-			player.setAllowFlight(false);
+		if (!bPlayer.isElementToggled(Element.AIR) || !bPlayer.isToggled() || player.getLocation().getBlock().isLiquid()) {
 			clear();
 			return;
 		}
-		
-		if (player.getLocation().getBlock().isLiquid()) {
-			clear();
-			return;
+
+		if ((!player.isFlying() && !player.getCanPickupItems()) || player.getGameMode() == GameMode.CREATIVE) {
+			player.setCanPickupItems(true);
 		}
-		
+
 		player.setAllowFlight(true);
-		
-		if (active && toggled) {	
-			player.setGliding(true);
-			
-			if (player.isSneaking() && player.getFlySpeed() < maxSpeed) {
-				speed = speed + acceleration;
-				if (speed > maxSpeed) {
-					speed = maxSpeed;
-				}	
+
+		if (!active) return;
+
+		if (player.getCanPickupItems()) player.setCanPickupItems(false);
+
+		if (!toggled) return;
+
+		player.setGliding(true);
+
+		if (player.isSneaking() && player.getFlySpeed() < maxSpeed) {
+			speed = speed + acceleration;
+			if (speed > maxSpeed) {
+				speed = maxSpeed;
 			}
-			
-			player.setVelocity(player.getEyeLocation().getDirection().multiply(speed));
 		}
+
+		player.setVelocity(player.getEyeLocation().getDirection().multiply(speed));
 	}
 	
 	private void clear() {
@@ -58,6 +70,8 @@ public class FlightPassive extends FlightAbility implements AddonAbility, Passiv
 		toggled = false;
 		player.setFlying(false);
 		player.setGliding(false);
+		player.setAllowFlight(false);
+		player.setCanPickupItems(true);
 	}
 	
 	@Override
@@ -134,12 +148,11 @@ public class FlightPassive extends FlightAbility implements AddonAbility, Passiv
 		if (flying) {
 			active = true;
 			original = player.getFlySpeed();
-			pickup = player.getCanPickupItems();
 			player.setFlySpeed(flySpeed);
 			player.setCanPickupItems(false);
 		} else {
 			player.setFlySpeed(original);
-			player.setCanPickupItems(pickup);
+			player.setCanPickupItems(true);
 			active = false;
 		}
 		toggled = false;
